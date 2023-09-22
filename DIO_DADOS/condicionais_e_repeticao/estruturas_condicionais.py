51f04bc2a7aaa97dc96072959f3a4b64bcf8d165
RogerioTiuma <rogerio.tiuma@gmail.com>
2023-09-22 13:00:31 -0300

    introducaoCondicional2

---
+++ DIO_DADOS/condicionais_e_repeticao/estruturas_condicionais.py
@@ -0,0 +1,16 @@
+MAIOR_IDADE = 18
+
+idade = int(input("Informe sua idade:"))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+
+if idade < MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+else:
+    print("Maior de idade, pode tirar a CNH.")
+    
+    
