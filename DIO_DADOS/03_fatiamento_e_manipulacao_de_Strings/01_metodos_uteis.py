a3b6540a0bf38c058087abb880ff4dcd845c233d
RogerioTiuma <rogerio.tiuma@gmail.com>
2023-09-27 13:01:36 -0300

    organizacao_strings

---
+++ DIO_DADOS/03_fatiamento_e_manipulacao_de_Strings/01_metodos_uteis.py
@@ -0,0 +1,16 @@
+curso = "pYtHon"
+
+print(curso.upper())
+
+print(curso.lower())
+
+print(curso.title())
+
+curso = "    Python "
+
+#Remove espaço em branco à direita e esquerda
+print(curso.strip())
+
+print(curso.lstrip())
+
+print(curso.rstrip())
